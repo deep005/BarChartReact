77fe8db54183fe5fac35773a939478e60b0c5b0b
Deep Shanker <deep@deep.local>
2019-03-04 03:43:37 +0530

    Updates

---
+++ precache-manifest.5685cb50f8768ef00314e6eda3cf63d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a93306d9f2762301d03",
+    "url": "/BarChartReact/static/js/runtime~main.8a93306d.js"
+  },
+  {
+    "revision": "7f808b7427b282519bae",
+    "url": "/BarChartReact/static/js/main.7f808b74.chunk.js"
+  },
+  {
+    "revision": "cf748f81a3da6ba20ca5",
+    "url": "/BarChartReact/static/js/2.cf748f81.chunk.js"
+  },
+  {
+    "revision": "7f808b7427b282519bae",
+    "url": "/BarChartReact/static/css/main.375050ad.chunk.css"
+  },
+  {
+    "revision": "73d8f89d9b5f387f16bd981f5e8a90e8",
+    "url": "/BarChartReact/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/BarChartReact/precache-manifest.5685cb50f8768ef00314e6eda3cf63d3.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/BarChartReact/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
